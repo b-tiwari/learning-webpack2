90db9f07b6c12b91de5db723e35b6bc0f375f9f1
Bharat Tiwari <bharat.tiwari@rackspace.com>
2017-03-21 18:08:57 -0500

    change

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     entry: "./src/scripts/app.js",//path relative to this file
     output: {
-        filename: "./dist/bundle.js"//path relative to this file
+        filename: "./dist/app.bundle.js"//path relative to this file
     }
 }
