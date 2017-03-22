54382ad1f783cbf4ce0cfdf49a3042851653015c
Bharat Tiwari <bharat.tiwari@rackspace.com>
2017-03-21 22:29:50 -0500

    htmlwebpackplugin using template

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -9,6 +9,9 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             hash: true,
+            title: 'My Awesome application',
+            myPageHeader: 'Hello World',
+            template: './src/index.html',
             filename: './dist/index.html' //relative to root of the application
         })
    ]
